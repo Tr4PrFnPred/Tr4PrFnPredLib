7d2e427aceeeb2508e7f1ff80120d743b59be108
Derek Shao <shao.derek.now@gmail.com>
2021-02-03 11:25:13 -0500

    setup to add package data

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name='Tr4PrFnPredLib',
-      version='0.0.2',
+      version='0.0.3',
       description='Library for protein function prediction models',
       author='Tr4FnPr',
       author_email='derekshao@cmail.carleton.ca',
       packages=find_packages(),
+      include_package_data=True,
       install_requires=[
             'tensorflow-gpu',
             'torch',
